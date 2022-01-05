369d5115e3e36219f4174a81d49c2e0a6d956f2c
alexmercerind <saini123hitesh@gmail.com>
2022-01-05 18:31:55 +0530

    Feat: Initial high-level Player class

--- lib/libmpv.dart
+++ lib/libmpv.dart
@@ -4,5 +4,8 @@
 /// All rights reserved.
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 
+export 'package:libmpv/src/player.dart';
+export 'package:libmpv/src/models/media.dart';
+
 export 'package:libmpv/src/core/initializer.dart';
 export 'package:libmpv/src/core/utils.dart';
