59d82ca9176acfd5d67613bfdbd8507b117f70eb
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-01-09 19:59:54 +0530

    Feat: Inital shared library loader

---
+++ lib/src/dynamic_library.dart
@@ -0,0 +1,105 @@
+/// This file is a part of libmpv.dart (https://github.com/alexmercerind/libmpv.dart).
+///
+/// Copyright (c) 2022, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+
+import 'dart:io';
+import 'package:path/path.dart';
+
+/// Path to DLL or shared object of libmpv.
+late String libmpvDynamicLibrary;
+
+const String kWindowsDynamicLibrary = 'mpv-1.dll';
+const String kLinuxDynamicLibrary = 'libmpv.so';
+
+/// ## MPV
+///
+/// Dart bindings to libmpv.
+/// [MPV.initialize] initializes the library.
+///
+/// ```dart
+/// void main() {
+///   await MPV.initialize();
+///   final player = Player();
+///   player.open(
+///     [
+///       Media('https://www.example.com/music.mp3'),
+///       Media('file://C:/documents/video.mp4'),
+///     ],
+///   );
+///   player.play();
+/// }
+/// ```
+///
+abstract class MPV {
+  static Future<void> initialize({String? dynamicLibrary}) async {
+    if (dynamicLibrary != null) {
+      libmpvDynamicLibrary = dynamicLibrary;
+      return;
+    }
+    if (Platform.isWindows) {
+      final mpvPath = Platform.environment['MPV_PATH'];
+      final envPath = Platform.environment['PATH'];
+      if (mpvPath != null) {
+        if (await File(join(mpvPath, kWindowsDynamicLibrary)).exists()) {
+          libmpvDynamicLibrary = join(mpvPath, kWindowsDynamicLibrary);
+          return;
+        }
+      }
+      if (envPath != null) {
+        final paths = envPath.split(';');
+        for (var path in paths) {
+          if (await File(join(path, kWindowsDynamicLibrary)).exists()) {
+            libmpvDynamicLibrary = join(path, kWindowsDynamicLibrary);
+            return;
+          }
+        }
+      }
+      if (await File(join(Platform.script.path, kWindowsDynamicLibrary))
+          .exists()) {
+        libmpvDynamicLibrary =
+            join(Platform.script.path, kWindowsDynamicLibrary);
+        return;
+      }
+      if (await File(join(
+              (Platform.resolvedExecutable.split('\\')..removeLast())
+                  .join('\\'),
+              kWindowsDynamicLibrary))
+          .exists()) {
+        libmpvDynamicLibrary = join(
+            (Platform.resolvedExecutable.split('\\')..removeLast()).join('\\'),
+            kWindowsDynamicLibrary);
+        return;
+      }
+      throw Exception(
+          'Cannot find mpv-1.dll in your system %PATH%. One way to deal with this is to ship mpv-1.dll '
+          'with your script or compiled executable in the same directory.');
+    }
+    if (Platform.isLinux) {
+      if (await File('/usr/lib/x86_64-linux-gnu/$kLinuxDynamicLibrary')
+          .exists()) {
+        libmpvDynamicLibrary =
+            '/usr/lib/x86_64-linux-gnu/$kLinuxDynamicLibrary';
+        return;
+      }
+      if (await File(join(Platform.script.path, kWindowsDynamicLibrary))
+          .exists()) {
+        libmpvDynamicLibrary =
+            join(Platform.script.path, kWindowsDynamicLibrary);
+        return;
+      }
+      if (await File(join(
+              (Platform.resolvedExecutable.split('/')..removeLast()).join('/'),
+              kWindowsDynamicLibrary))
+          .exists()) {
+        libmpvDynamicLibrary = join(
+            (Platform.resolvedExecutable.split('/')..removeLast()).join('/'),
+            kWindowsDynamicLibrary);
+        return;
+      }
+      throw Exception(
+          'Cannot find libmpv in the usual places. Depending on your distro, you may try installing an mpv-devel or libmpv-dev package.');
+    }
+  }
+}
