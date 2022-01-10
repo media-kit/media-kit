e8701bb5921406a300ee8ead11813edd90e66a1e
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-01-11 01:28:29 +0530

    Example: Use network media streams

--- example/minimal.dart
+++ example/minimal.dart
@@ -1,55 +1,31 @@
-import 'dart:io';
-import 'dart:async';
-import 'dart:convert';
-
 import 'package:libmpv/libmpv.dart';
 
 Future<void> main() async {
-  var player = Player('/usr/lib/x86_64-linux-gnu/libmpv.so');
-  while (true) {
-    var choice = await input();
-    var arg = (choice.split(' ')..removeAt(0)).join(' ');
-    if (choice.contains('open')) {
-      player.open(
-        [Media(arg)],
-        play: true,
-      );
-    }
-    if (choice.contains('play')) {
-      player.play();
-    }
-    if (choice.contains('pause')) {
-      player.pause();
-    }
-    if (choice.contains('seek')) {
-      player.seek(Duration(seconds: int.parse(arg)));
-    }
-    if (choice.contains('back')) {
-      player.back();
-    }
-    if (choice.contains('next')) {
-      player.next();
-    }
-    if (choice.contains('jump')) {
-      player.jump(int.parse(arg));
-    }
-  }
-}
-
-Stream<String> stream = stdin
-    .transform(utf8.decoder)
-    .transform(const LineSplitter())
-    .asBroadcastStream();
-
-Future<String> input() async {
-  var completer = Completer<String>();
-  var listener = stream.listen((line) {
-    if (!completer.isCompleted) {
-      completer.complete(line);
-    }
-  });
-  return completer.future.then((line) {
-    listener.cancel();
-    return line.trim();
-  });
+  await MPV.initialize();
+  final player = Player()
+    ..streams.playlist.listen((event) {
+      print(event);
+    })
+    ..streams.index.listen((event) {
+      print(event);
+    })
+    ..streams.position.listen((event) {
+      print(event);
+    });
+  player.open(
+    [
+      Media(
+        'https://p.scdn.co/mp3-preview/2312e9b4429d32218bf18778afb4dca0b25ac3f5?cid=a46f5c5745a14fbf826186da8da5ecc3',
+      ),
+      Media(
+        'https://p.scdn.co/mp3-preview/feb4812ccf46114e71085c2e1a5159b1b992e9c3?cid=a46f5c5745a14fbf826186da8da5ecc3',
+      ),
+      Media(
+        'https://p.scdn.co/mp3-preview/e2f3e698deaf0f09ed49436a7ab6abf6a989edfc?cid=a46f5c5745a14fbf826186da8da5ecc3',
+      ),
+      Media(
+        'https://p.scdn.co/mp3-preview/3f794acc211c14f5bbaef6ff44aa57b02b69bd9d?cid=a46f5c5745a14fbf826186da8da5ecc3',
+      ),
+    ],
+  );
 }
