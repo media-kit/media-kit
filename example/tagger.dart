01c7d6c27877835d9932db148eafd07204d52797
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-01-13 11:14:55 +0530

    Examples: Add tagger.dart

---
+++ example/tagger.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:libmpv/libmpv.dart';
+
+Future<void> main() async {
+  await MPV.initialize();
+  final tagger = Tagger();
+  Map<String, String> metadata;
+  metadata = await tagger.parse(
+    Media('https://alexmercerind.github.io/music.m4a'),
+    cover: File('cover.jpg'),
+  );
+  println(metadata);
+  metadata = await tagger.parse(
+    Media('https://alexmercerind.github.io/audio.ogg'),
+    cover: null,
+  );
+  println(metadata);
+}
+
+void println(dynamic object) =>
+    print(JsonEncoder.withIndent('    ').convert(object));
