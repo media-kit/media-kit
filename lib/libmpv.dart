f6fa23fe12f51068cc5b4bcefba242c6f5fe568b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-01-13 11:16:59 +0530

    Implement Tagger class

--- lib/libmpv.dart
+++ lib/libmpv.dart
@@ -5,6 +5,7 @@
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 
 export 'package:libmpv/src/player.dart';
+export 'package:libmpv/src/tagger.dart';
 export 'package:libmpv/src/models/media.dart';
 export 'package:libmpv/src/dynamic_library.dart';
 
