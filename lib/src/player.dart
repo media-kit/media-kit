4a95c1a1c2432b80fffb83d3f071887c5315b6f9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-03-11 19:52:17 +0530

    fix: Player.play behavior when no opening with play: false

--- lib/src/player.dart
+++ lib/src/player.dart
@@ -162,7 +162,7 @@ class Player {
       generated.mpv_format.MPV_FORMAT_INT64,
       pos.cast(),
     );
-    if (pos.value == 0 || state.isCompleted) {
+    if (pos.value <= 0 || state.isCompleted) {
       jump(0);
     }
     calloc.free(name);
