a68cca9c4d8ff56f2822a19ed4449ba254dba8c2
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-05-07 15:49:41 +0000

    update examples to follow updated API

--- example/player.dart
+++ example/player.dart
@@ -6,22 +6,26 @@ Future<void> main() async {
     ..streams.playlist.listen((event) {
       print(event);
     })
-    ..streams.index.listen((event) {
+    ..streams.playlist.listen((event) {
       print(event);
     })
     ..streams.position.listen((event) {
       print(event);
     });
-  player.open([
-    Media(
-        'https://p.scdn.co/mp3-preview/2312e9b4429d32218bf18778afb4dca0b25ac3f5?cid=a46f5c5745a14fbf826186da8da5ecc3'),
-    Media(
-        'https://p.scdn.co/mp3-preview/feb4812ccf46114e71085c2e1a5159b1b992e9c3?cid=a46f5c5745a14fbf826186da8da5ecc3'),
-    Media(
-        'https://p.scdn.co/mp3-preview/e2f3e698deaf0f09ed49436a7ab6abf6a989edfc?cid=a46f5c5745a14fbf826186da8da5ecc3'),
-    Media(
-        'https://p.scdn.co/mp3-preview/3f794acc211c14f5bbaef6ff44aa57b02b69bd9d?cid=a46f5c5745a14fbf826186da8da5ecc3'),
-  ]);
+  player.open(
+    Playlist(
+      [
+        Media(
+            'https://p.scdn.co/mp3-preview/2312e9b4429d32218bf18778afb4dca0b25ac3f5?cid=a46f5c5745a14fbf826186da8da5ecc3'),
+        Media(
+            'https://p.scdn.co/mp3-preview/feb4812ccf46114e71085c2e1a5159b1b992e9c3?cid=a46f5c5745a14fbf826186da8da5ecc3'),
+        Media(
+            'https://p.scdn.co/mp3-preview/e2f3e698deaf0f09ed49436a7ab6abf6a989edfc?cid=a46f5c5745a14fbf826186da8da5ecc3'),
+        Media(
+            'https://p.scdn.co/mp3-preview/3f794acc211c14f5bbaef6ff44aa57b02b69bd9d?cid=a46f5c5745a14fbf826186da8da5ecc3'),
+      ],
+    ),
+  );
   player.volume = 50.0;
   player.rate = 1.0;
 }
