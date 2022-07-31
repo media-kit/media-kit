fc90af15bab4c104eb5a9528be8073428332a119
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-07-31 23:27:18 +0530

    feat: add asset:// support & decouple URI cleanup

--- lib/src/models/media.dart
+++ lib/src/models/media.dart
@@ -4,6 +4,10 @@
 /// All rights reserved.
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+
 Map<String, Media> medias = {};
 
 /// ## Media
@@ -15,7 +19,7 @@ Map<String, Media> medias = {};
 ///
 class Media {
   /// URI of the [Media].
-  final String uri;
+  late final String uri;
 
   /// Additional optional user data.
   final dynamic extras;
@@ -28,17 +32,59 @@ class Media {
   /// ```
   ///
   Media(
-    this.uri, {
+    String uri, {
     this.extras,
   }) {
-    medias[() {
-      // Match with format retrieved by `mpv_get_property`.
-      if (uri.startsWith('file')) {
-        return Uri.parse(uri).toFilePath().replaceAll('\\', '/');
-      } else {
-        return uri;
+    this.uri = getCleanedURI(uri);
+    medias[this.uri] = this;
+  }
+
+  static String getCleanedURI(String uri) {
+    // Match with format retrieved by `mpv_get_property`.
+    // Only applicable on Windows.
+    if (uri.startsWith('file://')) {
+      return Uri.parse(uri).toFilePath().replaceAll('\\', '/');
+    }
+    // Only applicable on Windows.
+    else if (uri.contains('\\') && Platform.isWindows) {
+      return uri.replaceAll('\\', '/');
+    }
+    // Handle `asset://` separately.
+    else if (uri.startsWith('asset://')) {
+      if (Platform.isWindows || Platform.isLinux) {
+        return path.join(
+          path.dirname(Platform.resolvedExecutable),
+          'data',
+          'flutter_assets',
+          uri.split('asset://').last,
+        );
+      } else if (Platform.isMacOS) {
+        return path.join(
+          path.dirname(Platform.resolvedExecutable),
+          '..',
+          'Frameworks',
+          'App.framework',
+          'Resources',
+          'flutter_assets',
+          uri.split('asset://').last,
+        );
+      } else if (Platform.isIOS) {
+        return path.join(
+          path.dirname(Platform.resolvedExecutable),
+          'Frameworks',
+          'App.framework',
+          'flutter_assets',
+          uri.split('asset://').last,
+        );
       }
-    }()] = this;
+      throw UnimplementedError(
+        'asset:// is not supported on ${Platform.operatingSystem}',
+      );
+    }
+    // Other kinds of URIs e.g. HTTP, FTP, etc. are directly fed into libmpv.
+    else {
+      return uri;
+    }
   }
 
   @override
