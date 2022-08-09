877e7a90ef078f8eab8e6fb1d1e11ffa30d6c7ed
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-08-09 18:44:13 +0530

    refactor: plugins impl

--- lib/src/plugins/youtube.dart
+++ lib/src/plugins/youtube.dart
@@ -1,50 +1,55 @@
+/// This file is a part of media_kit (https://github.com/alexmercerind/media_kit).
+///
+/// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
 import 'dart:io';
 import 'dart:async';
 import 'dart:math' as math;
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-const String _kRequestAuthority = 'www.youtube.com';
-const String _kRequestKey = 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8';
-const Map<String, String> _kRequestHeaders = {
-  'accept': '*/*',
-  'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
-  'content-type': 'application/json',
-  'dpr': '2',
-  'sec-ch-ua-arch': '',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'same-origin',
-  'sec-fetch-site': 'same-origin',
-  'x-origin': 'https://www.youtube.com',
-  'x-youtube-client-name': '67',
-  'x-youtube-client-version': '1.20210823.00.00',
-};
-const Map<String, dynamic> _kRequestPayload = {
-  'context': {
-    'client': {
-      'clientName': 'ANDROID',
-      'clientScreen': 'EMBED',
-      'clientVersion': '16.43.34',
-    },
-    'thirdParty': {
-      'embedUrl': 'https://www.youtube.com',
-    },
-  },
-};
-
+/// Simple implementation that provides support for YouTube URLs playback inside [Player].
+///
+/// To make use of it's functionality, pass `yt` argument as `true` when creating a new
+/// [Player] for use.
+///
+/// ```dart
+/// final playerWithYouTubePlaybackSupport = Player(yt: true);
+/// ```
+///
+/// The methods of this class should be only accessed through the [instance] singleton.
+///
 class YouTube {
-  YouTube() {
-    _port = math.Random().nextInt((1 << 16) - 1);
-    _create().catchError((exception, stacktrace) {
-      print(exception);
-      print(stacktrace);
-    });
+  /// Public getter for [YouTube] class singleton.
+  static YouTube get instance {
+    _instance ??= YouTube._();
+    return _instance!;
   }
 
-  Future<void> close() => _server.close();
+  /// Keeps private [YouTube] object instance.
+  static YouTube? _instance;
 
-  Future<String> _stream(String id) async {
-    await _completer.future;
+  YouTube._() {
+    port = math.Random().nextInt(1 << 16);
+    create().catchError(
+      (exception, stacktrace) {
+        print(exception);
+        print(stacktrace);
+      },
+    );
+  }
+
+  /// Disposes the singleton instance of [YouTube] class.
+  Future<void> close() {
+    _instance = null;
+    return server.close();
+  }
+
+  /// Returns media stream URL for the given video [id].
+  ///
+  Future<String> stream(String id) async {
+    await completer.future;
     final response = await http.post(
       Uri.https(
         _kRequestAuthority,
@@ -81,78 +86,79 @@ class YouTube {
     return (opus ?? aac ?? mp4)!;
   }
 
-  Future<void> _create() async {
-    _server = HttpServer.listenOn(
-      await ServerSocket.bind('127.0.0.1', _port),
+  /// Creates the [_server].
+  ///
+  Future<void> create() async {
+    server = HttpServer.listenOn(
+      await ServerSocket.bind(
+        '127.0.0.1',
+        port,
+      ),
     );
-    _server.listen((request) async {
-      switch (request.uri.path) {
-        case '/youtube':
-          {
-            request.response.headers.set(
-              'location',
-              await _stream(request.uri.queryParameters['id']!),
-            );
-            request.response.statusCode = 302;
-            request.response.close();
+    server.listen(
+      (request) async {
+        switch (request.uri.path) {
+          case '/youtube':
+            {
+              request.response.headers.set(
+                'location',
+                await stream(
+                  request.uri.queryParameters['id']!,
+                ),
+              );
+              request.response.statusCode = HttpStatus.movedTemporarily;
+              request.response.close();
+              break;
+            }
+          default:
             break;
-          }
-        default:
-          break;
-      }
-    });
-    _completer.complete();
+        }
+      },
+    );
+    completer.complete();
   }
 
-  final _completer = Completer();
-  late final HttpServer _server;
+  /// Avoids any possible race condition of being asked to serve a URL before creation of [HttpServer].
+  ///
+  final completer = Completer();
+
+  /// The [HttpServer] which is started to redirect YouTube URLs to a locally running server on `127.0.0.1`.
+  ///
+  /// Then, that local URL redirects to the actual playable media stream (extracted through custom implementation)
+  /// using 302 status code i.e. [HttpStatus.movedTemporarily].
+  ///
+  late final HttpServer server;
+
+  /// Port on which [HttpServer] is set to listening.
+  /// Automatically generated randomly between 0 && 65535.
+  ///
+  late final int port;
 }
 
-late int _port;
-
-abstract class Plugins {
-  static Uri redirect(Uri uri) {
-    final string = uri.toString();
-    if ((string.contains('.youtube') ||
-            string.contains('youtu.be') ||
-            string.contains('/youtu')) &&
-        string.contains('/')) {
-      final path = 'http://127.0.0.1:$_port/youtube?id=';
-      if (string.contains('/watch?v=')) {
-        return Uri.parse(
-            path + string.substring(string.indexOf('=') + 1).split('&').first);
-      } else {
-        return Uri.parse(path + string.substring(string.indexOf('.be/') + 4));
-      }
-    }
-    return uri;
-  }
-
-  static bool isWebMedia(Uri uri) =>
-      (uri.toString().contains('.youtube') ||
-          uri.toString().contains('youtu.be') ||
-          uri.toString().contains('/youtu')) &&
-      uri.toString().contains('/');
-
-  static String artwork(
-    Uri uri, {
-    bool small = false,
-  }) {
-    final string = uri.toString();
-    if (isWebMedia(uri)) {
-      // YouTube links of the form https://www.youtube.com/watch?v=abc.
-      if (string.contains('/watch?v=')) {
-        return 'https://i.ytimg.com/vi/${string.substring(string.indexOf('=') + 1).split('&').first}/${small ? 'mqdefault' : 'maxresdefault'}.jpg';
-      }
-      // Re-directed YouTube links of the form https://127.0.0.1:6900/youtube?id=abc.
-      else if (string.contains('127.0.0.1')) {
-        return 'https://i.ytimg.com/vi/${uri.toString().split('=').last}/${small ? 'mqdefault' : 'maxresdefault'}.jpg';
-      }
-      // YouTube links of the form https://youtu.be/abc/.
-      else {
-        return 'https://i.ytimg.com/vi/${string.substring(string.indexOf('.be/') + 4).split('/').first}/${small ? 'mqdefault' : 'maxresdefault'}.jpg';
-      }
-    }
-    throw FormatException();
-  }
-}
+const String _kRequestAuthority = 'www.youtube.com';
+const String _kRequestKey = 'AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8';
+const Map<String, String> _kRequestHeaders = {
+  'accept': '*/*',
+  'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
+  'content-type': 'application/json',
+  'dpr': '2',
+  'sec-ch-ua-arch': '',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'same-origin',
+  'sec-fetch-site': 'same-origin',
+  'x-origin': 'https://www.youtube.com',
+  'x-youtube-client-name': '67',
+  'x-youtube-client-version': '1.20210823.00.00',
+};
+const Map<String, dynamic> _kRequestPayload = {
+  'context': {
+    'client': {
+      'clientName': 'ANDROID',
+      'clientScreen': 'EMBED',
+      'clientVersion': '16.43.34',
+    },
+    'thirdParty': {
+      'embedUrl': 'https://www.youtube.com',
+    },
+  },
+};
