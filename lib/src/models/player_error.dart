fbb759456298bc0a5e9ff41f4e2989ebd2a688d8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-02 19:29:20 +0530

    feat: add PlayerError, PlayerState & PlayerStreams model(s)

---
+++ lib/src/models/player_error.dart
@@ -0,0 +1,19 @@
+/// This file is a part of media_kit (https://github.com/alexmercerind/media_kit).
+///
+/// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+
+/// An error experienced by [Player] during playback.
+class PlayerError {
+  /// Error code.
+  final int code;
+
+  /// Error message.
+  final String message;
+
+  PlayerError(
+    this.code,
+    this.message,
+  );
+}
