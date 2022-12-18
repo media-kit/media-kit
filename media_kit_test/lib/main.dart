4080c3cfb95a9bab730ad967123a1464b7e634f9
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-18 19:25:00 +0530

    chore: test application initial commit

---
+++ media_kit_test/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SizedBox.shrink(),
+    );
+  }
+}
---
+++ media_kit_test/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <media_kit_core_video/media_kit_core_video_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MediaKitCoreVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitCoreVideoPluginCApi"));
+}
