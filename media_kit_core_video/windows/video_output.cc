1c3722891cf283b4f469394deee92ba4dd12ddcc
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-23 19:29:47 +0530

    feat: scaffold VideoOutput

---
+++ media_kit_core_video/windows/video_output.cc
@@ -0,0 +1,32 @@
+// This file is a part of media_kit
+// (https://github.com/alexmercerind/media_kit).
+//
+// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+// All rights reserved.
+// Use of this source code is governed by MIT license that can be found in the
+// LICENSE file.
+
+#include "video_output.h"
+
+#include <algorithm>
+
+VideoOutput::VideoOutput(int64_t handle,
+                         std::optional<int32_t> width,
+                         std::optional<int32_t> height,
+                         IDXGIAdapter* adapter)
+    : handle_(reinterpret_cast<mpv_handle*>(handle)),
+      width_(width_),
+      height_(height),
+      adapter_(adapter) {}
+
+int64_t VideoOutput::GetVideoWidth() {
+  int64_t width = 0;
+  mpv_get_property(handle_, "width", MPV_FORMAT_INT64, &width);
+  return width;
+}
+
+int64_t VideoOutput::GetVideoHeight() {
+  int64_t height = 0;
+  mpv_get_property(handle_, "height", MPV_FORMAT_INT64, &height);
+  return height;
+}
---
+++ media_kit_core_video/windows/video_output.h
@@ -0,0 +1,34 @@
+// This file is a part of media_kit
+// (https://github.com/alexmercerind/media_kit).
+//
+// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+// All rights reserved.
+// Use of this source code is governed by MIT license that can be found in the
+// LICENSE file.
+
+#include <optional>
+
+#include <client.h>
+
+#include "angle_surface_manager.h"
+
+class VideoOutput {
+ public:
+  VideoOutput(int64_t handle,
+              std::optional<int32_t> width,
+              std::optional<int32_t> height,
+              IDXGIAdapter* adapter);
+
+  ~VideoOutput();
+
+ private:
+  int64_t GetVideoWidth();
+
+  int64_t GetVideoHeight();
+
+  mpv_handle* handle_ = nullptr;
+  std::optional<int32_t> height_ = std::nullopt;
+  std::optional<int32_t> width_ = std::nullopt;
+  IDXGIAdapter* adapter_ = nullptr;
+  ANGLESurfaceManager* surface_manager_ = nullptr;
+};
