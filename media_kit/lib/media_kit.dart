0339c05dcc28c3b8b598c84d3b75c592e84fb18a
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-29 00:07:13 +0530

    fix: export(s)

--- media_kit/lib/media_kit.dart
+++ media_kit/lib/media_kit.dart
@@ -7,19 +7,9 @@
 // ignore_for_file: camel_case_types
 
 export 'package:media_kit/src/player.dart';
-export 'package:media_kit/src/tagger.dart';
 export 'package:media_kit/src/platform_player.dart';
-export 'package:media_kit/src/platform_tagger.dart';
 
 export 'package:media_kit/src/models/media.dart';
 export 'package:media_kit/src/models/playlist.dart';
 export 'package:media_kit/src/models/playlist_mode.dart';
 export 'package:media_kit/src/models/tagger_metadata.dart';
-
-// Public `typedef` declarations for making private platform specific APIs accessible.
-
-import 'package:media_kit/src/libmpv/player.dart' as libmpv;
-import 'package:media_kit/src/libmpv/tagger.dart' as libmpv;
-
-typedef libmpv_Player = libmpv.Player;
-typedef libmpv_Tagger = libmpv.Tagger;
