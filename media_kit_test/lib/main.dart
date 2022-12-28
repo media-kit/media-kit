a421688d863ea359f76697991383535d39f34119
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-12-28 21:36:18 +0530

    feat: improve test application

--- media_kit_test/lib/main.dart
+++ media_kit_test/lib/main.dart
@@ -26,7 +26,7 @@ class MyScreen extends StatefulWidget {
 }
 
 class _MyScreenState extends State<MyScreen> {
-  // Create a [Player] instance from `package:media_kit`
+  // Create a [Player] instance from `package:media_kit`.
   final Player player = Player();
   // Reference to the [VideoController] instance.
   VideoController? controller;
@@ -38,39 +38,82 @@ class _MyScreenState extends State<MyScreen> {
       // Create a [VideoController] instance from `package:media_kit_core_video`.
       // Pass the [handle] of the [Player] from `package:media_kit` to the [VideoController] constructor.
       controller = await VideoController.create(player.handle);
-      // Play some [Media].
-      player.open(
-        Playlist(
-          [
-            Media(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
-            ),
-            Media(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4',
-            ),
-            Media(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4',
-            ),
-            Media(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
-            ),
-            Media(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4',
-            ),
-          ],
-        ),
-      );
       setState(() {});
     });
   }
 
+  List<Widget> get assets => [
+        for (int i = 0; i < 5; i++)
+          ListTile(
+            title: Text('video_$i.mp4'),
+            onTap: () {
+              player.open(
+                Playlist(
+                  [
+                    Media(
+                      'asset://assets/video_$i.mp4',
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+      ];
+
   @override
   Widget build(BuildContext context) {
+    final horizontal =
+        MediaQuery.of(context).size.width > MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text('package:media_kit'),
       ),
-      body: Video(controller: controller),
+      body: horizontal
+          ? Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Card(
+                      elevation: 8.0,
+                      clipBehavior: Clip.antiAlias,
+                      margin: const EdgeInsets.all(32.0),
+                      child: Video(
+                        controller: controller,
+                      ),
+                    ),
+                  ),
+                ),
+                const VerticalDivider(width: 1.0, thickness: 1.0),
+                Expanded(
+                  flex: 1,
+                  child: ListView(
+                    children: assets,
+                  ),
+                ),
+              ],
+            )
+          : ListView(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width * 9.0 / 16.0,
+                  child: Card(
+                    elevation: 8.0,
+                    clipBehavior: Clip.antiAlias,
+                    margin: const EdgeInsets.all(32.0),
+                    child: Video(
+                      controller: controller,
+                    ),
+                  ),
+                ),
+                const Divider(height: 1.0, thickness: 1.0),
+                ...assets,
+              ],
+            ),
     );
   }
 }
