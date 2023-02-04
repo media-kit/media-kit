2df8c584ca33cf8aea92700c5e83723c40b4d052
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-02-04 10:49:33 +0000

    fix: linux fallback pixel buffer rendering

--- media_kit_video/linux/video_output.cc
+++ media_kit_video/linux/video_output.cc
@@ -21,6 +21,7 @@ struct _VideoOutput {
   GdkGLContext* gdk_gl_context;
   guint8* pixel_buffer;
   TextureSW* texture_sw;
+  GMutex mutex; /* Only used in S/W rendering. */
   mpv_handle* handle;
   mpv_render_context* render_context;
   gint64 width;
@@ -36,8 +37,6 @@ G_DEFINE_TYPE(VideoOutput, video_output, G_TYPE_OBJECT)
 static void video_output_dispose(GObject* object) {
   VideoOutput* self = VIDEO_OUTPUT(object);
   self->destroyed = TRUE;
-  fl_texture_registrar_mark_texture_frame_available(
-      self->texture_registrar, FL_TEXTURE(self->texture_gl));
   // H/W
   if (self->texture_gl) {
     fl_texture_registrar_unregister_texture(self->texture_registrar,
@@ -53,6 +52,7 @@ static void video_output_dispose(GObject* object) {
     g_object_unref(self->texture_sw);
   }
   mpv_render_context_free(self->render_context);
+  g_mutex_clear(&self->mutex);
   g_print("media_kit: VideoOutput: video_output_dispose: %ld\n",
           (gint64)self->handle);
   G_OBJECT_CLASS(video_output_parent_class)->dispose(object);
@@ -75,6 +75,7 @@ static void video_output_init(VideoOutput* self) {
   self->texture_update_callback_context = NULL;
   self->texture_registrar = NULL;
   self->destroyed = FALSE;
+  g_mutex_init(&self->mutex);
 }
 
 VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
@@ -82,6 +83,7 @@ VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
                               gint64 width,
                               gint64 height) {
   g_print("media_kit: VideoOutput: video_output_new: %ld\n", handle);
+  gboolean is_hardware_acceleration_enabled = FALSE;
   VideoOutput* self = VIDEO_OUTPUT(g_object_new(video_output_get_type(), NULL));
   self->texture_registrar = texture_registrar;
   self->handle = (mpv_handle*)handle;
@@ -90,7 +92,6 @@ VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
   GError* error = NULL;
   self->gdk_gl_context =
       gdk_window_create_gl_context(gdk_get_default_root_window(), &error);
-  gboolean is_hardware_acceleration_enabled = FALSE;
   if (error == NULL) {
     // OpenGL context must be made current before creating mpv render context.
     gdk_gl_context_realize(self->gdk_gl_context, &error);
@@ -102,17 +103,16 @@ VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
         // Request H/W decoding.
         mpv_set_option_string(self->handle, "hwdec", "auto");
         mpv_opengl_init_params gl_init_params{
-            [](auto, auto name) -> void* {
+            [](auto, auto name) {
               GdkDisplay* display = gdk_display_get_default();
               if (GDK_IS_WAYLAND_DISPLAY(display)) {
                 return (void*)eglGetProcAddress(name);
               }
               if (GDK_IS_X11_DISPLAY(display)) {
-                return (void*)(intptr_t)glXGetProcAddressARB(
-                    (const GLubyte*)name);
+                return (void*)glXGetProcAddressARB((const GLubyte*)name);
               }
               g_assert_not_reached();
-              return NULL;
+              return (void*)NULL;
             },
             NULL,
         };
@@ -130,10 +130,8 @@ VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
                 if (self->destroyed) {
                   return;
                 }
-                FlTextureRegistrar* texture_registrar = self->texture_registrar;
-                FlTexture* texture = FL_TEXTURE(self->texture_gl);
                 fl_texture_registrar_mark_texture_frame_available(
-                    texture_registrar, texture);
+                    self->texture_registrar, FL_TEXTURE(self->texture_gl));
               },
               self);
           is_hardware_acceleration_enabled = TRUE;
@@ -151,52 +149,58 @@ VideoOutput* video_output_new(FlTextureRegistrar* texture_registrar,
   if (!is_hardware_acceleration_enabled) {
     // H/W rendering failed somewhere down the line. Fallback to S/W rendering.
     self->pixel_buffer = g_new0(guint8, SW_RENDERING_PIXEL_BUFFER_SIZE);
+    self->texture_gl = NULL;
+    self->gdk_gl_context = NULL;
     self->texture_sw = texture_sw_new(self);
-    mpv_render_param params[] = {
-        {MPV_RENDER_PARAM_API_TYPE, (void*)MPV_RENDER_API_TYPE_SW},
-        {MPV_RENDER_PARAM_INVALID, (void*)0},
-    };
-    if (mpv_render_context_create(&self->render_context, self->handle,
-                                  params) == 0) {
-      mpv_render_context_set_update_callback(
-          self->render_context,
-          [](void* data) {
-            // Usage on single-thread is not a concern with pixel buffers unlike
-            // OpenGL. Let's perform rendering on a separate thread, pixel
-            // buffers are already "heavy".
-            g_thread_new(
-                "mpv_render_context_set_update_callback",
-                [](gpointer data) -> gpointer {
-                  VideoOutput* self = (VideoOutput*)data;
-                  if (self->destroyed) {
+    if (fl_texture_registrar_register_texture(texture_registrar,
+                                              FL_TEXTURE(self->texture_sw))) {
+      mpv_render_param params[] = {
+          {MPV_RENDER_PARAM_API_TYPE, (void*)MPV_RENDER_API_TYPE_SW},
+          {MPV_RENDER_PARAM_INVALID, (void*)0},
+      };
+      if (mpv_render_context_create(&self->render_context, self->handle,
+                                    params) == 0) {
+        mpv_render_context_set_update_callback(
+            self->render_context,
+            [](void* data) {
+              // Usage on single-thread is not a concern with pixel buffers
+              // unlike OpenGL. So, I'd like to render on a separate thread for
+              // slowing the UI thread as little as possible.
+              // It's a pity that software rendering is feeling faster than
+              // hardware rendering due to fucked-up GTK.
+              g_thread_new(
+                  "mpv_render_context_set_update_callback",
+                  [](gpointer data) -> gpointer {
+                    VideoOutput* self = (VideoOutput*)data;
+                    if (self->destroyed) {
+                      return NULL;
+                    }
+                    g_mutex_lock(&self->mutex);
+                    gint64 width = video_output_get_width(self);
+                    gint64 height = video_output_get_height(self);
+                    if (width > 0 && height > 0) {
+                      gint32 size[]{(gint32)width, (gint32)height};
+                      gint32 pitch = 4 * (gint32)width;
+                      mpv_render_param params[]{
+                          {MPV_RENDER_PARAM_SW_SIZE, size},
+                          {MPV_RENDER_PARAM_SW_FORMAT, (void*)"rgb0"},
+                          {MPV_RENDER_PARAM_SW_STRIDE, &pitch},
+                          {MPV_RENDER_PARAM_SW_POINTER, self->pixel_buffer},
+                          {MPV_RENDER_PARAM_INVALID, (void*)0},
+                      };
+                      mpv_render_context_render(self->render_context, params);
+                      fl_texture_registrar_mark_texture_frame_available(
+                          self->texture_registrar,
+                          FL_TEXTURE(self->texture_sw));
+                    }
+                    g_mutex_unlock(&self->mutex);
                     return NULL;
-                  }
-                  gint64 width = video_output_get_width(self);
-                  gint64 height = video_output_get_height(self);
-                  if (width > 0 && height > 0) {
-                    gint32 size[]{(gint32)width, (gint32)height};
-                    gint32 pitch = 4 * size[0];
-                    guint8* pointer = self->pixel_buffer;
-                    mpv_render_param params[]{
-                        {MPV_RENDER_PARAM_SW_SIZE, size},
-                        {MPV_RENDER_PARAM_SW_FORMAT, (void*)"rgb0"},
-                        {MPV_RENDER_PARAM_SW_STRIDE, &pitch},
-                        {MPV_RENDER_PARAM_SW_POINTER, pointer},
-                        {MPV_RENDER_PARAM_INVALID, (void*)0},
-                    };
-                    mpv_render_context_render(self->render_context, params);
-                    FlTextureRegistrar* texture_registrar =
-                        self->texture_registrar;
-                    FlTexture* texture = FL_TEXTURE(self->texture_gl);
-                    fl_texture_registrar_mark_texture_frame_available(
-                        texture_registrar, texture);
-                  }
-                  return NULL;
-                },
-                data);
-          },
-          self);
-      g_print("media_kit: VideoOutput: Using S/W rendering.\n");
+                  },
+                  data);
+            },
+            self);
+        g_print("media_kit: VideoOutput: Using S/W rendering.\n");
+      }
     }
   }
 #endif
@@ -210,8 +214,8 @@ void video_output_set_texture_update_callback(
   self->texture_update_callback = texture_update_callback;
   self->texture_update_callback_context = texture_update_callback_context;
   // Notify initial dimensions as (1, 1) if |width| & |height| are 0 i.e.
-  // texture & video frame size is based on playing file's resolution. This will
-  // make sure that `Texture` widget on Flutter's widget tree is actually
+  // texture & video frame size is based on playing file's resolution. This
+  // will make sure that `Texture` widget on Flutter's widget tree is actually
   // mounted & |fl_texture_registrar_mark_texture_frame_available| actually
   // invokes the |TextureGL| or |TextureSW| callbacks. Otherwise it will be a
   // never ending deadlock where no video frames are ever rendered.
@@ -281,11 +285,11 @@ gint64 video_output_get_texture_id(VideoOutput* self) {
 }
 
 void video_output_notify_texture_update(VideoOutput* self) {
+  gint64 id = video_output_get_texture_id(self);
   gint64 width = video_output_get_width(self);
   gint64 height = video_output_get_height(self);
   gpointer context = self->texture_update_callback_context;
   if (self->texture_update_callback != NULL) {
-    self->texture_update_callback((gint64)self->texture_gl, width, height,
-                                  context);
+    self->texture_update_callback(id, width, height, context);
   }
 }
