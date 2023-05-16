ffb72f2674e24d2c259ddb995293574b3c84bb7b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-05-17 02:01:06 +0530

    refactor: utilize new VideoController API in Video widget

--- media_kit_video/lib/src/video/video_web.dart
+++ media_kit_video/lib/src/video/video_web.dart
@@ -3,60 +3,59 @@
 /// Copyright © 2021 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
 /// All rights reserved.
 /// Use of this source code is governed by MIT license that can be found in the LICENSE file.
-
 import 'package:flutter/widgets.dart';
 
 import 'package:media_kit_video/src/video_controller/video_controller.dart';
+import 'package:media_kit_video/src/video_controller/platform_video_controller.dart';
 
 /// {@template video}
 ///
 /// Video
 /// -----
-/// [Video] widget is used to display video output inside Flutter widget tree.
+/// [Video] widget is used to display video output.
 ///
 /// Use [VideoController] to initialize & handle the video rendering.
 ///
 /// **Example:**
 ///
 /// ```dart
-/// final player = Player();
-/// VideoController? controller;
-///
-/// @override
-/// void initState() {
-///   super.initState();
-///   Future.microtask(() async {
-///     controller = await VideoController.create(player.handle);
-///     setState(() {});
-///   });
+/// class MyScreen extends StatefulWidget {
+///   const MyScreen({Key? key}) : super(key: key);
+///   @override
+///   State<MyScreen> createState() => MyScreenState();
 /// }
 ///
-/// @override
-/// void dispose() {
-///   Future.microtask(() async {
-///     await controller?.dispose();
-///     await player.dispose();
-///   });
-/// }
+/// class MyScreenState extends State<MyScreen> {
+///   late final player = Player();
+///   late final controller = VideoController(player);
 ///
-/// @override
-/// Widget build(BuildContext context) {
-///   return Scaffold(
-///     body: Center(
-///       /// Use [Video] widget to display video output.
-///       child: Video(
+///   @override
+///   void initState() {
+///     super.initState();
+///     player.open(Media('https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4'));
+///   }
+///
+///   @override
+///   void dispose() {
+///     player.dispose();
+///     super.dispose();
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       body: Video(
 ///         controller: controller,
 ///       ),
-///     ),
-///   );
+///     );
+///   }
 /// }
-///
 /// ```
 ///
 /// {@endtemplate}
 class Video extends StatefulWidget {
   /// The [VideoController] reference to control this [Video] output & connect with [Player] from `package:media_kit`.
-  final VideoController? controller;
+  final VideoController controller;
 
   /// Height of this viewport.
   final double? width;
@@ -109,43 +108,46 @@ class _VideoState extends State<Video> {
         child: FittedBox(
           alignment: widget.alignment,
           fit: widget.fit,
-          child: controller == null
-              ? const SizedBox.shrink()
-              : ValueListenableBuilder<int?>(
-                  valueListenable: controller.id,
-                  builder: (context, id, _) {
-                    return ValueListenableBuilder<Rect?>(
-                      valueListenable: controller.rect,
-                      builder: (context, rect, _) {
-                        if (id != null && rect != null) {
-                          return SizedBox(
-                            // Apply aspect ratio if provided.
-                            width: aspectRatio == null
-                                ? rect.width
-                                : rect.height * aspectRatio,
-                            height: rect.height,
-                            child: Stack(
-                              children: [
-                                const SizedBox(),
-                                Positioned.fill(
-                                  child: Texture(
-                                    textureId: id,
-                                    filterQuality: widget.filterQuality,
+          child: ValueListenableBuilder<PlatformVideoController?>(
+            valueListenable: controller.notifier,
+            builder: (context, notifier, _) => notifier == null
+                ? const SizedBox.shrink()
+                : ValueListenableBuilder<int?>(
+                    valueListenable: notifier.id,
+                    builder: (context, id, _) {
+                      return ValueListenableBuilder<Rect?>(
+                        valueListenable: notifier.rect,
+                        builder: (context, rect, _) {
+                          if (id != null && rect != null) {
+                            return SizedBox(
+                              // Apply aspect ratio if provided.
+                              width: aspectRatio == null
+                                  ? rect.width
+                                  : rect.height * aspectRatio,
+                              height: rect.height,
+                              child: Stack(
+                                children: [
+                                  const SizedBox(),
+                                  Positioned.fill(
+                                    child: Texture(
+                                      textureId: id,
+                                      filterQuality: widget.filterQuality,
+                                    ),
                                   ),
-                                ),
-                                HtmlElementView(
-                                  viewType:
-                                      'com.alexmercerind.media_kit_video.$id',
-                                )
-                              ],
-                            ),
-                          );
-                        }
-                        return const SizedBox.shrink();
-                      },
-                    );
-                  },
-                ),
+                                  HtmlElementView(
+                                    viewType:
+                                        'com.alexmercerind.media_kit_video.$id',
+                                  )
+                                ],
+                              ),
+                            );
+                          }
+                          return const SizedBox.shrink();
+                        },
+                      );
+                    },
+                  ),
+          ),
         ),
       ),
     );
