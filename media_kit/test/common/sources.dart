9a3b0db9127dfeec50cccbfb9d982f6aeed4b63a
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-05-19 23:56:51 +0530

    test: initial Player test for Media & Playlist open, HTTP headers & extras

--- media_kit/test/common/sources.dart
+++ media_kit/test/common/sources.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 
@@ -8,13 +9,13 @@ class _Sources {
   _Sources._();
 
   final List<String> network = [
-    'https://user-images.githubusercontent.com/28951144/229373695-22f88f13-d18f-4288-9bf1-c3e078d83722.mp4',
     'https://user-images.githubusercontent.com/28951144/229373709-603a7a89-2105-4e1b-a5a5-a6c3567c9a59.mp4',
     'https://user-images.githubusercontent.com/28951144/229373716-76da0a4e-225a-44e4-9ee7-3e9006dbc3e3.mp4',
     'https://user-images.githubusercontent.com/28951144/229373718-86ce5e1d-d195-45d5-baa6-ef94041d0b90.mp4',
     'https://user-images.githubusercontent.com/28951144/229373720-14d69157-1a56-4a78-a2f4-d7a134d7c3e9.mp4',
   ];
   final List<String> file = [];
+  final List<Uint8List> bytes = [];
 
   Future<void> prepare() async {
     if (_prepared) return;
@@ -27,6 +28,7 @@ class _Sources {
       );
       await File(destination).writeAsBytes(response.bodyBytes);
       file.add(destination);
+      bytes.add(response.bodyBytes);
     }
     // Force forward slashes for Windows.
     for (int i = 0; i < file.length; i++) {
---
+++ media_kit/test/src/player/libmpv/core/player_test.dart
@@ -0,0 +1,375 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:collection';
+import 'package:test/test.dart';
+import 'package:collection/collection.dart';
+
+import 'package:media_kit/src/media_kit.dart';
+import 'package:media_kit/src/player/player.dart';
+import 'package:media_kit/src/models/playlist.dart';
+import 'package:media_kit/src/models/media/media.dart';
+import 'package:media_kit/src/player/libmpv/player/player.dart';
+
+import '../../../../common/sources.dart';
+
+void main() {
+  setUp(() async {
+    await sources.prepare();
+    MediaKit.ensureInitialized();
+  });
+  test(
+    'player-platform',
+    () {
+      final player = Player();
+      expect(
+        player.platform,
+        isA<libmpvPlayer>(),
+      );
+    },
+  );
+  test(
+    'player-handle',
+    () {
+      final player = Player();
+      expect(
+        player.handle,
+        completes,
+      );
+    },
+  );
+  test(
+    'player-open-playable-media',
+    () async {
+      final player = Player();
+
+      final expectPlaylist = expectAsync1(
+        (value) {
+          print(value);
+          expect(value, isA<Playlist>());
+          final playlist = value as Playlist;
+          expect(
+            playlist.index,
+            0,
+          );
+          expect(
+            ListEquality().equals(playlist.medias, [Media(sources.file[0])]),
+            true,
+          );
+        },
+      );
+
+      player.streams.playlist.listen((e) {
+        expectPlaylist(e);
+      });
+
+      await player.open(Media(sources.file[0]));
+    },
+  );
+  test(
+    'player-open-playable-playlist',
+    () async {
+      final player = Player();
+
+      final expectPlaylist = expectAsync2(
+        (value, i) {
+          print(value);
+          expect(value, isA<Playlist>());
+          final playlist = value as Playlist;
+          expect(playlist.index, i);
+          expect(
+            ListEquality().equals(
+              playlist.medias,
+              [
+                for (int i = 0; i < sources.file.length; i++)
+                  Media(
+                    sources.file[i],
+                    extras: {
+                      'i': i.toString(),
+                    },
+                  ),
+              ],
+            ),
+            true,
+          );
+        },
+        count: sources.file.length,
+      );
+
+      player.streams.playlist.listen(
+        (e) {
+          if (e.index >= 0) {
+            expectPlaylist(e, e.index);
+          }
+        },
+      );
+
+      await player.open(
+        Playlist(
+          [
+            for (int i = 0; i < sources.file.length; i++)
+              Media(
+                sources.file[i],
+                extras: {
+                  'i': i.toString(),
+                },
+              ),
+          ],
+        ),
+      );
+    },
+    timeout: Timeout(const Duration(minutes: 1)),
+  );
+  test(
+    'player-open-playable-media-extras',
+    () async {
+      final player = Player();
+
+      final expectExtras = expectAsync1(
+        (value) {
+          print(value);
+          expect(value, isA<Map<String, String>>());
+          final extras = value as Map<String, String>;
+          expect(
+            MapEquality().equals(
+              extras,
+              {
+                'foo': 'bar',
+                'baz': 'qux',
+              },
+            ),
+            true,
+          );
+        },
+        count: 1,
+        max: -1,
+      );
+
+      await player.open(
+        Media(
+          sources.file[0],
+          extras: {
+            'foo': 'bar',
+            'baz': 'qux',
+          },
+        ),
+      );
+
+      player.streams.playlist.listen((e) {
+        expectExtras(e.medias.first.extras);
+      });
+    },
+  );
+  test(
+    'player-open-playable-playlist-extras',
+    () async {
+      final player = Player();
+
+      final expectExtras = expectAsync2(
+        (value, i) {
+          print(value);
+          expect(value, isA<Map<String, String>>());
+          final extras = value as Map<String, String>;
+          expect(
+            MapEquality().equals(
+              extras,
+              {
+                'i': i.toString(),
+              },
+            ),
+            true,
+          );
+        },
+        count: sources.file.length,
+      );
+
+      player.streams.playlist.listen(
+        (e) {
+          if (e.index >= 0) {
+            expectExtras(
+              e.medias[e.index].extras,
+              e.index,
+            );
+          }
+        },
+      );
+
+      await player.open(
+        Playlist(
+          [
+            for (int i = 0; i < sources.file.length; i++)
+              Media(
+                sources.file[i],
+                extras: {
+                  'i': i.toString(),
+                },
+              ),
+          ],
+        ),
+      );
+    },
+    timeout: Timeout(const Duration(minutes: 1)),
+  );
+  test(
+    'player-open-playable-media-http-headers',
+    () async {
+      final player = Player();
+
+      final address = '127.0.0.1';
+      final port = Random().nextInt(1 << 16 - 1);
+
+      final expectHTTPHeaders = expectAsync1(
+        (value) {
+          print(value);
+          expect(value, isA<HttpHeaders>());
+          final headers = value as HttpHeaders;
+
+          expect(headers.value('X-Foo'), 'Bar');
+          expect(headers.value('X-Baz'), 'Qux');
+        },
+        max: -1,
+      );
+
+      final expectPlaylist = expectAsync1(
+        (value) {
+          print(value);
+          expect(value, isA<Playlist>());
+          final playlist = value as Playlist;
+          expect(
+            ListEquality().equals(
+              playlist.medias,
+              [
+                Media(
+                  'http://$address:$port/0',
+                  httpHeaders: {
+                    'X-Foo': 'Bar',
+                    'X-Baz': 'Qux',
+                  },
+                ),
+              ],
+            ),
+            true,
+          );
+        },
+        max: -1,
+      );
+
+      final completed = HashSet<int>();
+
+      final socket = await ServerSocket.bind(address, port);
+      final server = HttpServer.listenOn(socket);
+      server.listen(
+        (e) async {
+          final i = int.parse(e.uri.path.split('/').last);
+          if (!completed.contains(i)) {
+            completed.add(i);
+            expectHTTPHeaders(e.headers);
+          }
+          final path = sources.file[i];
+          e.response.headers.add('Content-Type', 'video/mp4');
+          e.response.headers.add('Accept-Ranges', 'bytes');
+          File(path).openRead().pipe(e.response);
+        },
+      );
+
+      player.streams.playlist.listen((e) {
+        if (e.index >= 0) {
+          expectPlaylist(e);
+        }
+      });
+
+      await player.open(
+        Media(
+          'http://$address:$port/0',
+          httpHeaders: {
+            'X-Foo': 'Bar',
+            'X-Baz': 'Qux',
+          },
+        ),
+      );
+    },
+  );
+  test(
+    'player-open-playable-playlist-http-headers',
+    () async {
+      final player = Player();
+
+      final address = '127.0.0.1';
+      final port = Random().nextInt(1 << 16 - 1);
+
+      final expectHTTPHeaders = expectAsync2(
+        (value, i) {
+          print(value);
+          expect(value, isA<HttpHeaders>());
+          final headers = value as HttpHeaders;
+          expect(headers.value('X-Foo'), '$i');
+        },
+        count: sources.file.length,
+      );
+      final expectPlaylist = expectAsync2(
+        (value, i) {
+          print(value);
+          expect(value, isA<Playlist>());
+          final playlist = value as Playlist;
+          expect(playlist.index, i);
+          expect(
+            ListEquality().equals(
+              playlist.medias,
+              [
+                for (int i = 0; i < sources.file.length; i++)
+                  Media(
+                    'http://$address:$port/$i',
+                    httpHeaders: {
+                      'X-Foo': '$i',
+                    },
+                  ),
+              ],
+            ),
+            true,
+          );
+        },
+        count: sources.file.length,
+      );
+
+      final completed = HashSet<int>();
+
+      final socket = await ServerSocket.bind(address, port);
+      final server = HttpServer.listenOn(socket);
+      server.listen(
+        (e) async {
+          final i = int.parse(e.uri.path.split('/').last);
+          if (!completed.contains(i)) {
+            completed.add(i);
+            expectHTTPHeaders(e.headers, i);
+          }
+          final data = sources.bytes[i];
+          e.response.headers.contentLength = data.length;
+          e.response.headers.contentType = ContentType('video', 'mp4');
+          e.response.add(data);
+          await e.response.flush();
+          await e.response.close();
+        },
+      );
+
+      player.streams.playlist.listen((e) {
+        if (e.index >= 0) {
+          expectPlaylist(e, e.index);
+        }
+      });
+
+      await player.open(
+        Playlist(
+          [
+            for (int i = 0; i < sources.file.length; i++)
+              Media(
+                'http://$address:$port/$i',
+                httpHeaders: {
+                  'X-Foo': '$i',
+                },
+              ),
+          ],
+        ),
+      );
+    },
+    timeout: Timeout(const Duration(minutes: 1)),
+  );
+}
