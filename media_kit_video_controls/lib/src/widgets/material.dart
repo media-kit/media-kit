eaef6416e78684e3b06a4e838c74d8214b0282e8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-06-05 19:29:48 +0530

    feat: _MaterialSeekBar

--- media_kit_video_controls/lib/src/widgets/material.dart
+++ media_kit_video_controls/lib/src/widgets/material.dart
@@ -6,6 +6,7 @@
 // ignore_for_file: non_constant_identifier_names
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:media_kit_video/media_kit_video.dart';
 
 /// {@template material_video_controls}
@@ -28,11 +29,65 @@ Widget MaterialVideoControls(
 ///
 /// {@endtemplate}
 class MaterialVideoControlsThemeData {
-  const MaterialVideoControlsThemeData();
-}
+  /// [Duration] after which the controls will be hidden when there is no mouse movement.
+  final Duration controlsHoverDuration;
 
-const kVideoControlsMouseHoverDuration = Duration(seconds: 3);
-const kVideoControlsTransitionDuration = Duration(milliseconds: 150);
+  /// [Duration] for which the controls will be animated when shown or hidden.
+  final Duration controlsTransitionDuration;
+
+  /// [Duration] for which the seek bar will be animated when the user seeks.
+  final Duration seekBarTransitionDuration;
+
+  /// [Duration] for which the seek bar thumb will be animated when the user seeks.
+  final Duration seekBarThumbTransitionDuration;
+
+  /// Height of the seek bar.
+  final double seekBarHeight;
+
+  /// Height of the seek bar when hovered.
+  final double seekBarHoverHeight;
+
+  /// Height of the seek bar [Container].
+  final double seekBarContainerHeight;
+
+  /// Margin around the seek bar.
+  final EdgeInsets seekBarMargin;
+
+  /// [Color] of the seek bar.
+  final Color seekBarColor;
+
+  /// [Color] of the hovered section in the seek bar.
+  final Color seekBarHoverColor;
+
+  /// [Color] of the playback position section in the seek bar.
+  final Color seekBarPositionColor;
+
+  /// [Color] of the playback buffer section in the seek bar.
+  final Color seekBarBufferColor;
+
+  /// Size of the seek bar thumb.
+  final double seekBarThumbSize;
+
+  /// [Color] of the seek bar thumb.
+  final Color seekBarThumbColor;
+
+  const MaterialVideoControlsThemeData({
+    this.controlsHoverDuration = const Duration(seconds: 3),
+    this.controlsTransitionDuration = const Duration(milliseconds: 150),
+    this.seekBarTransitionDuration = const Duration(milliseconds: 300),
+    this.seekBarThumbTransitionDuration = const Duration(milliseconds: 150),
+    this.seekBarHeight = 3.2,
+    this.seekBarHoverHeight = 5.6,
+    this.seekBarContainerHeight = 36.0,
+    this.seekBarMargin = const EdgeInsets.symmetric(horizontal: 16.0),
+    this.seekBarColor = const Color(0x3DFFFFFF),
+    this.seekBarHoverColor = const Color(0x3DFFFFFF),
+    this.seekBarPositionColor = const Color(0xFFFF0000),
+    this.seekBarBufferColor = const Color(0x3DFFFFFF),
+    this.seekBarThumbSize = 12.0,
+    this.seekBarThumbColor = const Color(0xFFFF0000),
+  });
+}
 
 /// {@template material_video_controls_theme}
 ///
@@ -83,24 +138,24 @@ class _MaterialVideoControlsState extends State<_MaterialVideoControls> {
 
   Timer? _timer;
 
-  void onEnter() {
+  void onHover() {
     setState(() {
       visible = true;
     });
     _timer?.cancel();
-    _timer = Timer(kVideoControlsMouseHoverDuration, () {
+    _timer = Timer(widget.data.controlsHoverDuration, () {
       setState(() {
         visible = false;
       });
     });
   }
 
-  void onHover() {
+  void onEnter() {
     setState(() {
       visible = true;
     });
     _timer?.cancel();
-    _timer = Timer(kVideoControlsMouseHoverDuration, () {
+    _timer = Timer(widget.data.controlsHoverDuration, () {
       setState(() {
         visible = false;
       });
@@ -123,13 +178,15 @@ class _MaterialVideoControlsState extends State<_MaterialVideoControls> {
   Widget build(BuildContext context) {
     return MouseRegion(
       onHover: (_) => onHover(),
+      onEnter: (_) => onEnter(),
       onExit: (_) => onExit(),
-      child: Stack(
-        children: [
-          AnimatedOpacity(
-            opacity: visible ? 1.0 : 0.0,
-            duration: kVideoControlsTransitionDuration,
-            child: Container(
+      child: AnimatedOpacity(
+        opacity: visible ? 1.0 : 0.0,
+        duration: widget.data.controlsTransitionDuration,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
                   begin: Alignment.topCenter,
@@ -145,8 +202,232 @@ class _MaterialVideoControlsState extends State<_MaterialVideoControls> {
                 ),
               ),
             ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                _MaterialSeekBar(
+                  data: widget.data,
+                  controller: widget.controller,
+                ),
+                const SizedBox(height: 48.0),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MaterialSeekBar extends StatefulWidget {
+  final MaterialVideoControlsThemeData data;
+  final VideoController controller;
+  const _MaterialSeekBar({
+    Key? key,
+    required this.data,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  State<_MaterialSeekBar> createState() => _MaterialSeekBarState();
+}
+
+class _MaterialSeekBarState extends State<_MaterialSeekBar> {
+  bool hover = false;
+  bool click = false;
+  double slider = 0.0;
+
+  late bool playing = widget.controller.player.state.playing;
+  late Duration position = widget.controller.player.state.position;
+  late Duration duration = widget.controller.player.state.duration;
+  late Duration buffer = widget.controller.player.state.buffer;
+
+  final List<StreamSubscription> subscriptions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    subscriptions.addAll(
+      [
+        widget.controller.player.streams.playing.listen((event) {
+          setState(() {
+            playing = event;
+          });
+        }),
+        widget.controller.player.streams.completed.listen((event) {
+          setState(() {
+            position = Duration.zero;
+          });
+        }),
+        widget.controller.player.streams.position.listen((event) {
+          setState(() {
+            if (!click) position = event;
+          });
+        }),
+        widget.controller.player.streams.duration.listen((event) {
+          setState(() {
+            duration = event;
+          });
+        }),
+        widget.controller.player.streams.buffer.listen((event) {
+          setState(() {
+            buffer = event;
+          });
+        }),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    for (final subscription in subscriptions) {
+      subscription.cancel();
+    }
+    super.dispose();
+  }
+
+  void onPointerMove(PointerMoveEvent e, BoxConstraints constraints) {
+    final percent = e.localPosition.dx / constraints.maxWidth;
+    setState(() {
+      hover = true;
+      slider = percent.clamp(0.0, 1.0);
+    });
+  }
+
+  void onPointerDown() {
+    setState(() {
+      click = true;
+    });
+  }
+
+  void onPointerUp() {
+    setState(() {
+      click = false;
+    });
+    widget.controller.player.seek(duration * slider);
+  }
+
+  void onHover(PointerHoverEvent e, BoxConstraints constraints) {
+    final percent = e.localPosition.dx / constraints.maxWidth;
+    setState(() {
+      hover = true;
+      slider = percent.clamp(0.0, 1.0);
+    });
+  }
+
+  void onEnter(PointerEnterEvent e, BoxConstraints constraints) {
+    final percent = e.localPosition.dx / constraints.maxWidth;
+    setState(() {
+      hover = true;
+      slider = percent.clamp(0.0, 1.0);
+    });
+  }
+
+  void onExit(PointerExitEvent e, BoxConstraints constraints) {
+    setState(() {
+      hover = false;
+      slider = 0.0;
+    });
+  }
+
+  /// Returns the current playback position in percentage.
+  double get positionPercent {
+    if (position == Duration.zero || duration == Duration.zero) {
+      return 0.0;
+    } else {
+      final value = position.inMilliseconds / duration.inMilliseconds;
+      return value.clamp(0.0, 1.0);
+    }
+  }
+
+  /// Returns the current playback buffer position in percentage.
+  double get bufferPercent {
+    if (buffer == Duration.zero || duration == Duration.zero) {
+      return 0.0;
+    } else {
+      final value = buffer.inMilliseconds / duration.inMilliseconds;
+      return value.clamp(0.0, 1.0);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.none,
+      margin: widget.data.seekBarMargin,
+      child: LayoutBuilder(
+        builder: (context, constraints) => MouseRegion(
+          onHover: (e) => onHover(e, constraints),
+          onEnter: (e) => onEnter(e, constraints),
+          onExit: (e) => onExit(e, constraints),
+          child: Listener(
+            onPointerMove: (e) => onPointerMove(e, constraints),
+            onPointerDown: (e) => onPointerDown(),
+            onPointerUp: (e) => onPointerUp(),
+            child: Container(
+              color: Colors.transparent,
+              width: constraints.maxWidth,
+              height: widget.data.seekBarContainerHeight,
+              child: Stack(
+                alignment: Alignment.centerLeft,
+                children: [
+                  AnimatedContainer(
+                    width: constraints.maxWidth,
+                    height: hover
+                        ? widget.data.seekBarHoverHeight
+                        : widget.data.seekBarHeight,
+                    alignment: Alignment.centerLeft,
+                    duration: widget.data.seekBarThumbTransitionDuration,
+                    color: widget.data.seekBarColor,
+                    child: Stack(
+                      alignment: Alignment.centerLeft,
+                      children: [
+                        Container(
+                          width: constraints.maxWidth * slider,
+                          color: widget.data.seekBarHoverColor,
+                        ),
+                        Container(
+                          width: constraints.maxWidth * bufferPercent,
+                          color: widget.data.seekBarBufferColor,
+                        ),
+                        Container(
+                          width: click
+                              ? constraints.maxWidth * slider
+                              : constraints.maxWidth * positionPercent,
+                          color: widget.data.seekBarPositionColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    left: click
+                        ? (constraints.maxWidth -
+                                widget.data.seekBarThumbSize / 2) *
+                            slider
+                        : (constraints.maxWidth -
+                                widget.data.seekBarThumbSize / 2) *
+                            positionPercent,
+                    child: AnimatedContainer(
+                      width:
+                          hover || click ? widget.data.seekBarThumbSize : 0.0,
+                      height:
+                          hover || click ? widget.data.seekBarThumbSize : 0.0,
+                      duration: widget.data.seekBarThumbTransitionDuration,
+                      decoration: BoxDecoration(
+                        color: widget.data.seekBarThumbColor,
+                        borderRadius: BorderRadius.circular(
+                          widget.data.seekBarThumbSize / 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
