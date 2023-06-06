112f98108da8bc80082a70fd2e50ff36137217b2
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-06-06 12:49:24 +0530

    feat: FullScreenIdentifierInheritedWidget

--- media_kit_video_controls/lib/src/utils.dart
+++ media_kit_video_controls/lib/src/utils.dart
@@ -6,6 +6,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:media_kit_video/media_kit_video.dart';
 
+// ------------------------------------------------------------
+
 /// {@template video_controller_inherited_widget}
 ///
 /// Inherited widget which provides [VideoController] associated with the parent [Video] widget to descendant widgets.
@@ -40,6 +42,77 @@ class VideoControllerInheritedWidget extends InheritedWidget {
 VideoController controller(BuildContext context) =>
     VideoControllerInheritedWidget.of(context).controller;
 
+// ------------------------------------------------------------
+
+/// {@template fullscreen_identifier_inherited_widget}
+///
+/// Inherited widget used to identify whether some [Video] is in fullscreen or not.
+///
+/// {@endtemplate}
+class FullScreenIdentifierInheritedWidget extends InheritedWidget {
+  const FullScreenIdentifierInheritedWidget({
+    super.key,
+    required super.child,
+  });
+
+  static FullScreenIdentifierInheritedWidget? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<
+        FullScreenIdentifierInheritedWidget>();
+  }
+
+  static FullScreenIdentifierInheritedWidget of(BuildContext context) {
+    final FullScreenIdentifierInheritedWidget? result = maybeOf(context);
+    assert(result != null,
+        'No FullScreenIdentifierInheritedWidget found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(FullScreenIdentifierInheritedWidget oldWidget) =>
+      identical(this, oldWidget);
+}
+
+/// Whether fullscreen is enabled.
+bool isFullScreen(BuildContext context) =>
+    FullScreenIdentifierInheritedWidget.maybeOf(context) != null;
+
+/// Makes the [Video] present in the current [BuildContext] enter fullscreen.
+Future<void> enterFullScreen(BuildContext context) {
+  if (!isFullScreen(context)) {
+    return Navigator.of(context).push(
+      PageRouteBuilder(
+        pageBuilder: (_, __, ___) => FullScreenIdentifierInheritedWidget(
+          child: Video(
+            controller: controller(context),
+          ),
+        ),
+        transitionDuration: Duration.zero,
+        reverseTransitionDuration: Duration.zero,
+      ),
+    );
+  }
+  return Future.value();
+}
+
+/// Makes the [Video] present in the current [BuildContext] exit fullscreen.
+Future<void> exitFullScreen(BuildContext context) {
+  if (isFullScreen(context)) {
+    return Navigator.of(context).maybePop();
+  }
+  return Future.value();
+}
+
+/// Toggles fullscreen for the [Video] present in the current [BuildContext].
+Future<void> toggleFullScreen(BuildContext context) {
+  if (isFullScreen(context)) {
+    return exitFullScreen(context);
+  } else {
+    return enterFullScreen(context);
+  }
+}
+
+// ------------------------------------------------------------
+
 /// Extension methods for [Duration].
 extension DurationExtension on Duration {
   /// Returns a [String] representation of [Duration].
@@ -63,3 +136,5 @@ extension DurationExtension on Duration {
     }
   }
 }
+
+// ------------------------------------------------------------
