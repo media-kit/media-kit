4f240cd3f8f44164cf8b02c067642c5ea2d0026d
birros <birros@protonmail.com>
2023-06-27 20:43:48 +0200

    build: fix gitignore & update lock files

--- media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
+++ media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import media_kit_libs_macos_video
 import media_kit_video
 import path_provider_foundation
 import screen_brightness_macos
-import shared_preferences_foundation
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +16,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
