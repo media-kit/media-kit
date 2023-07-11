2bb26918784c3e3ccbe973d8c4dbcd667e953440
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-07-12 01:16:47 +0530

    fix: export subtitle_view.dart [skip ci]

--- media_kit_video/lib/media_kit_video.dart
+++ media_kit_video/lib/media_kit_video.dart
@@ -7,4 +7,6 @@
 export 'package:media_kit_video/src/video/video.dart';
 export 'package:media_kit_video/src/video_controller/video_controller.dart';
 
+export 'package:media_kit_video/src/subtitle/subtitle_view.dart';
+
 export 'package:media_kit_video/media_kit_video_controls/media_kit_video_controls.dart';
