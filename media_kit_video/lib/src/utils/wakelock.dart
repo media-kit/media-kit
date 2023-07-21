2c49c16beae20ec08f0c7d59c1075439b7c04784
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-07-22 02:49:21 +0530

    refactor(wakelock): use count instead of set for acquire/release

--- media_kit_video/lib/src/utils/wakelock.dart
+++ media_kit_video/lib/src/utils/wakelock.dart
@@ -1,42 +1,53 @@
+/// This file is a part of media_kit (https://github.com/alexmercerind/media_kit).
+///
+/// Copyright © 2021 & onwards, Julien Muret <birros@protonmail.com>.
+/// All rights reserved.
+/// Use of this source code is governed by MIT license that can be found in the LICENSE file.
+import 'package:flutter/widgets.dart';
 import 'package:wakelock_plus/wakelock_plus.dart';
 
-/// Manage the wakelock behavior for multiple instances.
+/// {@template wakelock}
+/// Wakelock
+/// --------
+///
+/// This class acquires & releases the wakelock based on reference count.
+///
+/// {@endtemplate}
 class Wakelock {
-  static final Set<Wakelock> _enabledInstances = {};
-  static bool _wakelockEnabled = false;
+  /// {@macro wakelock}
+  Wakelock();
 
   /// Marks the wakelock as enabled for this instance.
   void enable() {
-    _enabledInstances.add(this);
-    _updateWakeLock();
+    if (!_enabled) {
+      _enabled = true;
+      _count++;
+      _update();
+    }
   }
 
   /// Marks the wakelock as disabled for this instance.
   void disable() {
-    _enabledInstances.remove(this);
-    _updateWakeLock();
-  }
-
-  /// Toggles the wakelock status for this instance.
-  void toggle() {
-    if (_enabledInstances.contains(this)) {
-      _enabledInstances.remove(this);
-    } else {
-      _enabledInstances.add(this);
+    if (_enabled) {
+      _enabled = false;
+      _count--;
+      _update();
     }
-    _updateWakeLock();
   }
 
-  /// Updates the upstream wakelock status based on the enabled instances count.
-  static void _updateWakeLock() {
-    if (_enabledInstances.isNotEmpty && !_wakelockEnabled) {
+  /// Acquires the wakelock if enabled count is greater than 0.
+  void _update() {
+    if (_count > 0) {
       WakelockPlus.enable().catchError((_) {});
-      _wakelockEnabled = true;
-    } else if (_enabledInstances.isEmpty && _wakelockEnabled) {
+    } else {
       WakelockPlus.disable().catchError((_) {});
-      _wakelockEnabled = false;
     }
-
-    print("### wakelock: ${_enabledInstances.length}, $_wakelockEnabled");
+    debugPrint('media_kit: wakelock: _count = $_count');
   }
+
+  /// Whether the wakelock is enabled for this instance.
+  bool _enabled = false;
+
+  /// The number of [Wakelock] instances in enabled state.
+  static int _count = 0;
 }
