23baa4a031eae7746c77ade3d3acb8e513de1323
amirhassan-vm <hasanmahani08@gmail.com>
2024-04-22 10:37:21 +0330

    video_player upgraded

--- video_player_media_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ video_player_media_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,19 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import media_kit_libs_macos_video
-import media_kit_video
-import package_info_plus
 import path_provider_foundation
-import screen_brightness_macos
 import video_player_avfoundation
-import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
-  MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
