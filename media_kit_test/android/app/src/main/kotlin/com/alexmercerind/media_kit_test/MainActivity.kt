33b585fece1c6b4c575099b95f86cea10ec9473c
Predidit <34627277+Predidit@users.noreply.github.com>
2025-02-13 10:04:35 +0800

    fix android build warning && fix android test (flutter 3.29)

--- media_kit_test/android/app/src/main/kotlin/com/alexmercerind/media_kit_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.alexmercerind.media_kit_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ media_kit_test/android/app/src/main/kotlin/com/example/media_kit_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.media_kit_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
