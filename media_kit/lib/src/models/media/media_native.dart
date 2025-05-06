1c81111ad6cdb3f3299600c10ac0ffdb2bd31d4e
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2025-05-06 13:32:22 +0300

    fix: remove Uri.parse for fd check on android

--- media_kit/lib/src/models/media/media_native.dart
+++ media_kit/lib/src/models/media/media_native.dart
@@ -47,21 +47,22 @@ class Media extends Playable {
       if (ref[uri] == 0) {
         cache.remove(uri);
       }
+
       // content:// : Close the possible file descriptor on Android.
-      try {
-        if (Platform.isAndroid) {
-          final data = Uri.parse(uri);
-          if (data.isScheme('FD')) {
-            final fd = int.parse(data.authority);
-            if (fd > 0) {
-              await AndroidContentUriProvider.closeFileDescriptor(uri);
-            }
-          }
+      if (Platform.isAndroid &&
+          _kFileDescriptorScheme ==
+              uri
+                  .substring(
+                      0, _kFileDescriptorScheme.length.clamp(0, uri.length))
+                  .toLowerCase()) {
+        final fd =
+            int.tryParse(uri.substring(0, _kFileDescriptorScheme.length));
+        assert(fd != null, 'invalid file descriptor uri, fd is not an integer');
+        if (fd != null && fd > 0) {
+          await AndroidContentUriProvider.closeFileDescriptor(uri);
         }
-      } catch (exeception, stacktrace) {
-        print(exeception);
-        print(stacktrace);
       }
+
       // Media.memory : Delete the temporary file.
       try {
         if (memory) {
@@ -220,6 +221,9 @@ class Media extends Playable {
 
   /// URI scheme used to identify Android content.
   static const String _kContentScheme = 'content://';
+
+  /// URI scheme used to identify file descriptors.
+  static const String _kFileDescriptorScheme = 'fd://';
 
   /// Previously created [Media] instances.
   /// This [HashMap] is used to retrieve previously set [extras] & [httpHeaders].
