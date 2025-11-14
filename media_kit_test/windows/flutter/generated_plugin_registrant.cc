37f802ad8f8aef042cf46f0b9d5b3fa4102642ea
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2025-11-15 03:15:11 +0530

    build(deps): remove screen_brightness & volume_controller (#1317)

--- media_kit_test/windows/flutter/generated_plugin_registrant.cc
+++ media_kit_test/windows/flutter/generated_plugin_registrant.cc
@@ -8,13 +8,10 @@
 
 #include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
 #include <media_kit_video/media_kit_video_plugin_c_api.h>
-#include <volume_controller/volume_controller_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
   MediaKitVideoPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
-  VolumeControllerPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("VolumeControllerPluginCApi"));
 }
--- video_player_media_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ video_player_media_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import media_kit_video
 import package_info_plus
 import path_provider_foundation
 import video_player_avfoundation
-import volume_controller
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,6 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
-  VolumeControllerPlugin.register(with: registry.registrar(forPlugin: "VolumeControllerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
