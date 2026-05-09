6b9ff6df575081aba77c4c211504f7538fa4e551
Roman Laptev <carapacik@gmail.com>
2026-05-09 13:13:43 +0300

    Add workspace and update examples

--- media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
+++ media_kit_test/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import file_picker
 import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
-import path_provider_foundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +16,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
---
+++ video_player_media_kit/example/android/app/src/main/kotlin/com/alexmercerind/video_player_media_kit_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.alexmercerind.video_player_media_kit_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
